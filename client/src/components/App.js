a7f0ae065453de64f2729ca507dc8b0c3ccbdf0a
khuong291 <dkhuong291@gmail.com>
2018-08-20 23:00:31 +0700

    Create form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,10 +4,8 @@ import Header from "./Header";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 import Landing from "./Landing";
-import Dashboard from './Dashboard';
-import SurveyNew from './surveys/SurveyNew';
-
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
   componentDidMount() {
@@ -16,16 +14,14 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <BrowserRouter>
-          <div className="container">
-            <Header />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveyNew} />
-          </div>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import SurveyForm from './SurveyForm';
 
 class SurveyNew extends Component {
     render() {
         return (
             <div>
-                SurveyNew!
+                <SurveyForm />
             </div>
         )
     }
 }
+
+export default SurveyNew;
